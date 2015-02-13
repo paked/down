343558d9302f39eca92c7f7a08577b5bb9aee6b4
Harrison <harrison@theshoebridges.com>
2015-02-13 23:42:14 +1100

    parser_test: rewrite tests in a more extendable way

--- parser_test.go
+++ parser_test.go
@@ -1,47 +1,43 @@
 package down
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestParse(t *testing.T) {
-	p := Parser{}
-	source := "#Hello\nmy *name* is *harrison*\nHello"
-	p.Parse(source)
-	fmt.Println(p.String())
+func TestParseHeaders(t *testing.T) {
+	sources := []Source{{"#Hello", "<h1>Hello</h1>"}, {"##Hello", "<h2>Hello</h2>"}, {"####Hello", "<h4>Hello</h4>"}}
+	test(sources, "headers", t)
 }
 
 func TestParseLink(t *testing.T) {
-	p := Parser{}
-	source := "#Hello\nthis is a link [Go to google!](http://google.com)"
-	p.Parse(source)
-	fmt.Println(p.String())
+	sources := []Source{{"[Link](http://google.com)", `<p><a href='http://google.com'>Link</a></p>`}}
+	test(sources, "links", t)
 }
 
 func TestParseBold(t *testing.T) {
-	p := Parser{}
-	source := "**hello** ***hey how are you* how are you**"
-	p.Parse(source)
-	fmt.Println(p.String())
+	sources := []Source{{"**this is bold text**", "<p><b>this is bold text</b></p>"}}
+	test(sources, "bold", t)
 }
 
-func TestHeaderNode(t *testing.T) {
-	h := HeaderNode{}
-	h.Level = 1
-	fmt.Println(h.String())
+func TestParseItalics(t *testing.T) {
+	sources := []Source{{"*this is italic text*", "<p><i>this is italic text</i></p>"}}
+	test(sources, "italics", t)
 }
 
-func TestParseHeader(t *testing.T) {
-	p := Parser{}
-	source := "#Hello\n##Hello"
-	p.Parse(source)
-	fmt.Println(p.String())
+func TestParseUnorderedList(t *testing.T) {
+	sources := []Source{{"- One\n- Two", "<ul><li>One</li><li>Two</li></ul>"}}
+	test(sources, "unordered list", t)
 }
 
-func TestParseList(t *testing.T) {
-	p := Parser{}
-	source := "- I win\n- Hello"
-	p.Parse(source)
-	fmt.Println(p.String())
+type Source struct {
+	In, Out string
+}
+
+func test(sources []Source, name string, t *testing.T) {
+	for _, s := range sources {
+		p := Parse(s.In)
+		if r := (p == s.Out+"\n"); !r {
+			t.Errorf("Error with %v: %v is not %v", name, p, s.Out)
+		}
+	}
 }
